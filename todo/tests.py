ec22af69084c213abaeb423016c017995d4e42e5
1F10220127 <s1f102201278@inaid.org>
2024-07-04 11:22:45 +0900

    fix 2

--- todo/tests.py
+++ todo/tests.py
@@ -9,6 +9,7 @@ class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1+2, 3)
 
+
 class TakeModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -93,4 +94,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
-
